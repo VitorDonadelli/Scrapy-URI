ea36b067ec2f9fd54dfecc74aea2446dc8d5e8d3
donderileo <donderileo@gmail.com>
2020-10-08 20:23:58 -0300

    Commit inicial/ Quase metade do projeto

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Scrapy-URI
+
+A ideia desse repositório é criar um web scrapy que varre uma categoria de exercicios do [URI](https://www.urionlinejudge.com.br/judge/pt/categories) (ou todas) utilizando [Scrapy](https://scrapy.org/) para gerar um arquivo JSON com o número do exercício seguido da dificuldade do mesmo.
+
+## Etapas:
+
+Percorrer uma página coletando informações: *Done*
+Percorrer uma categoria coletando informações: *Done*
+Seletor de categorias: *Done*
+
+### Organização de informações
+
+Conseguir Listar:
+Fazer filtros:
+
+
+
+Só deus sabe o que mais
+
+## Tecnologias empregadas:
+
+* Scrapy
+* Python
+* Precisa de um pouco de conhecimento de HTML + CSS
+
+# Como utilizar:
+
+
+
---
+++ scrapy_uri/spiders/Exercicio_Spider.py
@@ -0,0 +1,31 @@
+import scrapy
+
+class WebScrapy(scrapy.Spider):
+    name = 'exercicios_uri' #Usado em: $ scrapy crawl name -o saida.json
+    i = int(input(
+            "Qual categoria quer fazer o crawl?\n[1] - Iniciante\n[2] - AD-HOC\n[3] - Strings\n[4] - Estruturas e Bibliotecas\n[5] - Matemática\n[6] - Paradigmas\n[7] - Grafos\n[8] - Geometria Computacional\n[9] - SQL\n"))
+
+    url_inicial = 'https://www.urionlinejudge.com.br/judge/pt/problems/index/'
+    
+    start_urls = [
+        url_inicial + str(i)
+    ]
+
+    def parse(self, response):
+        exercicios = response.css('#element .id a::text').getall() 
+        dificuldade = response.css('.small+ .tiny::text').getall() 
+
+        result = []
+        for i in range(0,len(exercicios)):
+            result.append(exercicios[i])
+            result.append(dificuldade[i])
+        
+
+        yield {
+            'Exercicios': result
+        }
+        
+        next_page = response.css('.next a::attr(href)').get()
+    
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
